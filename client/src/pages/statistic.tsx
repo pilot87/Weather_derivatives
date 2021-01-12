16493849ffdb8043e3695c1c3afa834af3df9f75
Alexey A Kostylev aka KernelExplorer <alexkos87@gmail.com>
2021-01-12 09:23:40 +0300

    Statistic painted

--- client/src/pages/statistic.tsx
+++ client/src/pages/statistic.tsx
@@ -13,14 +13,22 @@ export const Statistic = () => {
     {
         return {
             index: 0,
-            active: ['active', '', '']
+            active: ['green lighten-4', '', '']
         }
     }))
 
     const [active_tab, setActive_tab] = useState<{index: number, active: string[]}[]>(init)
 
-    const show = (s: StatDetail) =>
-        <table className="striped">
+    const show = (s: StatDetail, index: number) =>
+        <table className={[0].map((condition) => {
+            if(index % 2 === 0) {
+                console.log('yellow')
+                return "striped collection-item yellow lighten-4 grey-text text-darken-3"
+            } else {
+                console.log('green')
+                return "striped collection-item green lighten-4 grey-text text-darken-3"
+            }
+        })[0]}>
             <tbody>
                 <tr>
                     <td>Temperature</td>
@@ -49,12 +57,12 @@ export const Statistic = () => {
     let cities: any = useSelector((state: State) =>
         Object.entries(state.stats.city).map((city: [string, StatDetail[]], index: number) => {
             const tab_content: any = [
-                'Test 0',
+                'No futures',
                 'Quantity of futures: ' + city[1].length,
                 'Test 2',
             ]
             if (city[1].length !== 0) {
-                tab_content[0] = city[1].map(detail => show(detail))
+                tab_content[0] = city[1].map((detail, index2) => show(detail, index2))
             }
             if (active_tab[index] === undefined) {
                 window.location.replace('about')
@@ -71,19 +79,19 @@ export const Statistic = () => {
                                 <li className="tab" style={{cursor: 'pointer'}} onClick={() => {
                                     const ac = active_tab
                                     ac[index].index = 0
-                                    ac[index].active = ['active', '', '']
+                                    ac[index].active = ['green lighten-4', '', '']
                                     setActive_tab(ac)
-                                }}><p className={active_tab[index].active[0]}>Test 0</p></li>
+                                }}><p className={active_tab[index].active[0]}>Futures</p></li>
                                 <li className="tab" style={{cursor: 'pointer'}} onClick={() => {
                                     const ac = active_tab
                                     ac[index].index = 1
-                                    ac[index].active = ['', 'active', '']
+                                    ac[index].active = ['', 'green lighten-4', '']
                                     setActive_tab(ac)
-                                }}><p className={active_tab[index].active[1]}>Test 1</p></li>
+                                }}><p className={active_tab[index].active[1]}>Statistic</p></li>
                                 <li className="tab" style={{cursor: 'pointer'}} onClick={() => {
                                     const ac = active_tab
                                     ac[index].index = 2
-                                    ac[index].active = ['', '', 'active']
+                                    ac[index].active = ['', '', 'green lighten-4']
                                     setActive_tab(ac)
                                 }}><p className={active_tab[index].active[2]}>Test 2</p></li>
                             </ul>
@@ -101,9 +109,9 @@ export const Statistic = () => {
             <tr>
                 {cities.map((city: any) => {
                     if (city.length > i) {
-                        return <td>{city[i]}</td>
+                        return <td className="">{city[i]}</td>
                     } else {
-                        return <td>Blob</td>
+                        return <td></td>
                     }
                 })}
             </tr>
