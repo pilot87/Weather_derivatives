72d901f6c39364e3ab5d6bf6d7ea459c4870ad74
Alexey A Kostylev aka KernelExplorer <alexkos87@gmail.com>
2021-02-03 15:05:09 +0300

    app changed to run on UNIT 2 iteration, gitignore fixed

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,28 @@
 #!/home/web/.nvm/versions/node/v14.15.4/bin/node
+let {
+    createServer,
+    IncomingMessage,
+    ServerResponse,
+}: {
+    createServer: any,
+    IncomingMessage: any,
+    ServerResponse: any
+} = {
+    createServer: null,
+    IncomingMessage: null,
+    ServerResponse: null
+}
 
+if (process.env.NODE_ENV === 'production') {
+    const {v0, v1, v2,} = require('unit-http')
+    createServer = v0
+    IncomingMessage = v1
+    ServerResponse = v2
+    require('http').ServerResponse = ServerResponse
+    require('http').IncomingMessage = IncomingMessage
+}
+
+import express = require('express')
 export const {Router} = require('express')
 export const config = require('config')
 const path = require('path')
@@ -9,8 +32,6 @@ export const jwt = require('jsonwebtoken')
 
 import {forecast} from './shedule/weather.update'
 import {billing} from './shedule/billing.update'
-
-import express = require('express')
 
 const app: express.Application = express()
 
@@ -28,6 +49,15 @@ app.use('/api/profile', require('./routes/profile.routes'))
 app.use('/api/weather', require('./routes/weather.routes'))
 app.use('/api/derivative', require('./routes/derivative.routes'))
 
+if (process.env.NODE_ENV === 'production') {
+    console.log(__dirname)
+    app.use('/', express.static(path.join(__dirname, '../', 'client', 'build')))
+
+    app.get('*', (req: any, res: any) => {
+        res.sendFile(path.resolve(__dirname, '../', 'client', 'build', 'index.html'))
+    })
+}
+
 const PORT = config.get('port') || 5000
 
 const start = async () => {
@@ -39,44 +69,10 @@ const start = async () => {
             useFindAndModify: false
         })
         if (process.env.NODE_ENV === 'production') {
-            const {
-                createServer,
-                IncomingMessage,
-                ServerResponse,
-            } = require('unit-http')
-
-            require('http').ServerResponse = ServerResponse
-            require('http').IncomingMessage = IncomingMessage
-
-            const express = require('express')
-            const app = express()
-
-            app.use(express.json({ extended: true }))
-
-            app.use('/api/auth', require('./routes/auth.routes'))
-            app.use('/api/profile', require('./routes/profile.routes'))
-            app.use('/api/weather', require('./routes/weather.routes'))
-            app.use('/api/derivative', require('./routes/derivative.routes'))
-
-            app.use('/', express.static(path.join(__dirname, '../', 'client', 'build')))
-            app.get('*', (req: any, res: any) => {
-                res.sendFile(path.resolve(__dirname, '../', 'client', 'build', 'index.html'))
-            })
             createServer(app).listen(PORT, () => console.log(`App has been started on port ${PORT}...`))
         } else {
-            const express = require('express')
-            const app = express()
-
-            app.use(express.json({ extended: true }))
-
-            app.use('/api/auth', require('./routes/auth.routes'))
-            app.use('/api/profile', require('./routes/profile.routes'))
-            app.use('/api/weather', require('./routes/weather.routes'))
-            app.use('/api/derivative', require('./routes/derivative.routes'))
-
             app.listen(PORT, () => console.log(`App has been started on port ${PORT}...`))
         }
-
     } catch (e) {
         console.log('Server Error', e.message)
         process.exit(1)
