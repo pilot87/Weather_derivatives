9ea231889d92807faf6277d8985414cbb3563679
Alexey A Kostylev aka KernelExplorer <alexkos87@gmail.com>
2021-02-03 21:58:59 +0300

    install.sh fixed

--- install.sh
+++ install.sh
@@ -1,1 +1,5 @@
-#!/usr/bin/env bashnpm inpm install express --savenpm link unit-httpnpm run-script server:buildchmod +x app.js
+npm i
+npm install express --save
+npm link unit-http
+npm run-script server:build
+chmod +x app.js
