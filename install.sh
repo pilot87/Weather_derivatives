76e313c7c86a3e034698ccb6568c3ead2a92e2b6
Alexey A Kostylev aka KernelExplorer <alexkos87@gmail.com>
2021-02-10 18:19:13 +0300

    install script fixwd

--- install.sh
+++ install.sh
@@ -3,6 +3,6 @@ chmod 750 unit_ready.py
 npm install express --save
 npm link unit-http
 # argument of unit_ready script is root url
-./unit_ready.py /wf
+./unit_ready.py -u /wf -s unit
 npm run-script server:build
 chmod +x app.js
