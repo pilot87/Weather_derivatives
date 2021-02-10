d82e4911d02d4d9928419cc1d94c492d3f2a5551
Alexey A Kostylev aka KernelExplorer <alexkos87@gmail.com>
2021-02-11 01:02:07 +0300

    Navbar linkes fixed

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,14 +1,21 @@
 import React from 'react'
 import {NavLink} from 'react-router-dom'
 
+let base = ''
+if (process.env.NODE_ENV !== 'development') {
+    console.log('NODE_ENV')
+    // @ts-ignore
+    base = /\/[a-zA-Z0-9_]*/.exec(window.location.pathname)[0]
+}
+
 export const Navbar = ({auth}: any): any => {
     const user = auth.name
     if (user === '') return (
         <nav>
             <div className='nav-wrapper'>
-                <a href='' className='brand-logo'>Forecast Trading</a>
+                <a href={base} className='brand-logo'>Forecast Trading</a>
                 <ul id='nav-mobile' className='right hide-on-med-and-down'>
-                    <li><NavLink to='login'>Hello, Guest!</NavLink></li>
+                    <li><NavLink to={base + '/login'}>Hello, Guest!</NavLink></li>
                 </ul>
             </div>
         </nav>
@@ -19,10 +26,10 @@ export const Navbar = ({auth}: any): any => {
             <div className='nav-wrapper'>
                 <a href='' className='brand-logo'>Forecast Trading</a>
                 <ul id='nav-mobile' className='right hide-on-med-and-down'>
-                    <li><NavLink to='weather'>Weather</NavLink></li>
-                    <li><NavLink to='statistic'>Statistic</NavLink></li>
-                    <li><NavLink to='futures'>Futures</NavLink></li>
-                    <li><NavLink to='about'>Hello, {user}!</NavLink></li>
+                    <li><NavLink to={base + '/weather'}>Weather</NavLink></li>
+                    <li><NavLink to={base + '/statistic'}>Statistic</NavLink></li>
+                    <li><NavLink to={base + '/futures'}>Futures</NavLink></li>
+                    <li><NavLink to={base + '/about'}>Hello, {user}!</NavLink></li>
                 </ul>
             </div>
         </nav>
