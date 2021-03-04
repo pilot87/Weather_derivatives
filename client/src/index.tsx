8aa236ca73d34a0dc5894ca7523ad49cfdda0ddd
Alexey A Kostylev aka KernelExplorer <alexkos87@gmail.com>
2021-03-04 05:20:59 +0300

    Try to preload images

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -7,11 +7,13 @@ import App from './App'
 import {store} from './app/store'
 import {Provider} from 'react-redux'
 import * as serviceWorker from './serviceWorker'
+import {city_img} from './components/Images'
 
 import {updateBalance, regularUpdateBalance} from './features/auth/useAuth'
 import {regularUpdateRate} from './features/derivative/useDerivative'
 import {updateStats, regularUpdateStats} from './features/stats/useStats'
 import {updateWeather, regularUpdateWeather} from './features/weather/useWeather'
+
 
 const headRoot = document.head
 class Head extends React.Component {
@@ -24,38 +26,75 @@ export const history = createBrowserHistory({
     basename: process.env.PUBLIC_URL
 })
 
-const app = async () => {
-    store.dispatch(updateWeather())
-    store.dispatch(updateStats())
-    store.dispatch(updateBalance())
-    store.dispatch(regularUpdateWeather())
-    store.dispatch(regularUpdateRate())
-    store.dispatch(regularUpdateBalance())
-    store.dispatch(regularUpdateStats())
-
-    let base = ''
-    if (process.env.NODE_ENV !== 'development') {
-        console.log('NODE_ENV')
-        // @ts-ignore
-        base = /\/[a-zA-Z0-9_]*/.exec(window.location.pathname)[0]
-    }
-
-    ReactDOM.render(
-        <React.StrictMode>
-            <Head>
-                <link href='https://fonts.googleapis.com/icon?family=Material+Icons' rel='stylesheet'/>
-                <meta name='viewport' content='width=device-width, initial-scale=1.0'/>
-                <title>My Page</title>
-                <base href={base + '/'}/>
-            </Head>
-            <Provider store={store}>
-                <App/>
-            </Provider>
-        </React.StrictMode>,
-        document.getElementById('root')
-    )
-    serviceWorker.unregister()
-
+let base = ''
+if (process.env.NODE_ENV !== 'development') {
+    console.log('NODE_ENV')
+    // @ts-ignore
+    base = /\/[a-zA-Z0-9_]*/.exec(window.location.pathname)[0]
 }
 
-app()
+class Root extends React.Component {
+
+    componentDidMount() {
+        store.dispatch(updateWeather())
+        store.dispatch(updateStats())
+        store.dispatch(updateBalance())
+        store.dispatch(regularUpdateWeather())
+        store.dispatch(regularUpdateRate())
+        store.dispatch(regularUpdateBalance())
+        store.dispatch(regularUpdateStats())
+        city_img.forEach((picture) => {
+            const img = new Image()
+            img.src = picture
+        });
+    }
+
+    render(): React.ReactNode {
+        return (
+            <React.StrictMode>
+                <Head>
+                    <link href='https://fonts.googleapis.com/icon?family=Material+Icons' rel='stylesheet'/>
+                    <meta name='viewport' content='width=device-width, initial-scale=1.0'/>
+                    <title>My Page</title>
+                    <base href={base + '/'}/>
+                </Head>
+                <Provider store={store}>
+                    <App/>
+                </Provider>
+            </React.StrictMode>
+        )
+    }
+}
+
+ReactDOM.render(
+    <Root />,
+    document.getElementById('root')
+)
+
+// const app = async () => {
+//     store.dispatch(updateWeather())
+//     store.dispatch(updateStats())
+//     store.dispatch(updateBalance())
+//     store.dispatch(regularUpdateWeather())
+//     store.dispatch(regularUpdateRate())
+//     store.dispatch(regularUpdateBalance())
+//     store.dispatch(regularUpdateStats())
+//
+//     ReactDOM.render(
+//         <React.StrictMode>
+//             <Head>
+//                 <link href='https://fonts.googleapis.com/icon?family=Material+Icons' rel='stylesheet'/>
+//                 <meta name='viewport' content='width=device-width, initial-scale=1.0'/>
+//                 <title>My Page</title>
+//                 <base href={base + '/'}/>
+//             </Head>
+//             <Provider store={store}>
+//                 <App/>
+//             </Provider>
+//         </React.StrictMode>,
+//         document.getElementById('root')
+//     )
+//     serviceWorker.unregister()
+// }
+//
+// app()
